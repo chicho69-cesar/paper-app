1992466edbb33cee8743a3262064e190de2daece
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-04-09 00:10:48 -0600

    :sparkles: Add dialogs undismissables

---
+++ src/Examples/Dialogs/UndismissableDialog.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Button, Portal, Dialog, MD2Colors } from 'react-native-paper';
+
+import { TextComponent } from './DialogTextComponent';
+
+type UDialogProps = {
+  visible: boolean;
+  close:   () => void;
+};
+
+const UndismissableDialog = ({
+  visible,
+  close,
+}: UDialogProps): JSX.Element => (
+  <Portal>
+    <Dialog onDismiss={close} visible={visible} dismissable={false}>
+      <Dialog.Title>Alert</Dialog.Title>
+
+      <Dialog.Content>
+        <TextComponent>This is an undismissable dialog!!</TextComponent>
+      </Dialog.Content>
+
+      <Dialog.Actions>
+        <Button color={MD2Colors.teal500} disabled>
+          Disagree
+        </Button>
+        
+        <Button onPress={close}>Agree</Button>
+      </Dialog.Actions>
+    </Dialog>
+  </Portal>
+);
+
+export default UndismissableDialog;
